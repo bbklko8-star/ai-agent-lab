713a4cd6d5366e4bb565aa13b0d5e84a3ea94d7d
bbklko8-star <bbklko8@gmail.com>
2026-03-23 11:28:02 -0400

    added real AI

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import google.generativeai as genai
 
 app = FastAPI()
+
+genai.configure(api_key="AIzaSyC2-CArnZdUhmgVMRJjsGH167ywSBhFyxE")
+
+model = genai.GenerativeModel("gemini-pro")
 
 @app.get("/")
 def home():
@@ -8,21 +13,9 @@ def home():
 
 @app.get("/ask")
 def ask(question: str):
+    response = model.generate_content(question)
     
-    # Simple AI logic (rule-based)
-    if "ai" in question.lower():
-        answer = "AI (Artificial Intelligence) is the simulation of human intelligence in machines."
-    
-    elif "python" in question.lower():
-        answer = "Python is a popular programming language used in AI, web development, and automation."
-    
-    elif "network" in question.lower():
-        answer = "A network connects multiple devices to share data and resources."
-    
-    else:
-        answer = "Good question! I will answer this properly when we connect real AI."
-
     return {
         "question": question,
-        "answer": answer
+        "answer": response.text
     }
